22f8dd13cbc70f852478a974dd65cc5aa7125a6d
Dennis yee <ykkadmin+github@gmail.com>
2014-12-01 10:10:38 -0800

    replace connect with browsersync, added conditional for dev vs prod builds

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var sass = require('gulp-ruby-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
-var connect = require('gulp-connect');
+//var connect = require('gulp-connect');
 var rm = require('gulp-rimraf');
 var filter = require('gulp-filter');
 var inject = require('gulp-inject');
@@ -19,28 +19,39 @@ var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
+var gulpif = require('gulp-if');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var minimist = require('minimist');
 
 
-var devDir = './app';
 var srcDir = './src';
+var devDir = './app';
 var prodDir = './dist';
+
+var targetDir = devDir;
+
+var knownOptions = {
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'development' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
 
 var paths = {
   html : {
     src  : srcDir + '/index.html',
-    dev  : devDir,
-    prod : prodDir
+    target : targetDir
   },
   style : {
     src  : srcDir + '/css/*.sass',
-    dev  : devDir + '/css',
-    prod : prodDir + '/css'
+    target : targetDir + "/css"
   },
 
   templates : {
     src  : srcDir + '/templates/*.hbs',
-    dev  : devDir + '/js',
-    prod : prodDir + '/js',
+    target : targetDir + "/js",
     name : "templates.js"
   },
   js: {
@@ -49,91 +60,116 @@ var paths = {
   main : {
     name : 'main.js',
     src  : srcDir + '/js/main.js',
-    dev  : devDir +'/js',
-    prod : prodDir + '/js'
+    target : targetDir + "/js"
   },
   ele: {
     src : srcDir + '/jsx/*.jsx'
   }
 };
 
+gulp.task('war', [ 'build' ], function(){
 
-gulp.task('run',[ 'html-dev', 'style-dev', 'main-dev', 'templates-dev', 'watch'], function () {
-  connect.server({
-    root: [ devDir ],
-    port: 8000,
-    livereload: true
+});
+
+
+gulp.task('run',[ 'build', 'watch'], function () {
+  //connect.server({
+  //  root: [ devDir ],
+  //  port: 8000,
+  //  livereload: true
+  //});
+  browserSync({
+    server: {
+      baseDir: targetDir
+    }
   });
 });
 
-
 // Rerun the task when a file changes
-gulp.task('watch', ['clean-dev'], function() {
-  gulp.watch(paths.html.src, ['html-dev']);
-  gulp.watch(paths.style.src, ['style-dev']);
-  gulp.watch(paths.main.src, ['main-dev']);
-  gulp.watch(paths.ele.src,  ['main-dev']);
+gulp.task('watch', ['clean'], function() {
+  gulp.watch(paths.html.src,  ['build', browserSync.reload]);
+  gulp.watch(paths.style.src, ['style', browserSync.reload]);
+  gulp.watch(paths.main.src,  ['main', browserSync.reload]);
+  gulp.watch(paths.ele.src,   ['main', browserSync.reload]);
 });
 
+gulp.task('clean', function() {
+  console.log( "Env :  " + options.env );
 
-
-
-gulp.task('clean-dev', function() {
-  return gulp.src( devDir + "/*" )
+  if( options.env == 'production' ){
+    targetDir = prodDir;
+  }
+  return gulp.src( targetDir + "/*" )
    .pipe( rm ())
-   .pipe( gulp.dest( devDir ));
+   .pipe( gulp.dest( targetDir ));
 });
 
-
-
-
-
-gulp.task('html-dev', [ 'jsdep-dev', 'cssdep-dev', 'style-dev','main-dev', 'templates-dev' ],function () {
+gulp.task('build', [ 'jsdep', 'cssdep', 'style','main', 'templates' ],function () {
   var target = gulp.src(paths.html.src);
-  var imp = gulp.src(devDir + "/js/jquery.js", {read: false});
-  var sources = gulp.src([devDir + "/js/*.js", "!" + devDir + "/js/jquery.js", devDir + "/css/*.css"], {read: false});
+  var jquery = "jquery.js";
+  if( options.env == "production"){
+    jquery = "jquery.min.js";
+    console.log( "html : " + options.env + " jquery : " + jquery  );
+  }
+  var imp = gulp.src(targetDir + "/js/" + jquery, {read: false});
+  var sources = gulp.src([targetDir + "/js/*.js", "!" + targetDir + "/js/" + jquery, targetDir + "/css/*.css"], {read: false});
   return target
-  .pipe(inject(imp,{starttag: '<!-- inject:head:{{ext}} -->', relative: true, ignorePath: '../app/'}))
-  .pipe(inject(sources, {relative: true, ignorePath: '../app/'}))
-  .pipe(gulp.dest(paths.html.dev));
+  .pipe(inject(imp,{starttag: '<!-- inject:head:{{ext}} -->', relative: true, ignorePath: ['../app/','../dist']}))
+  .pipe(inject(sources, {relative: true, ignorePath: ['../app/','../dist']}))
+  .pipe(gulp.dest( targetDir ));
 
 });
 
-gulp.task('jsdep-dev', ['clean-dev'],function(){
+gulp.task('jsdep', ['clean'],function(){
   var jsFilter = filter("*.js");
   return gulp.src(mainBowerFiles())
    .pipe( jsFilter )
-   .pipe(gulp.dest(devDir + "/js"));
+   .pipe(gulpif(options.env === 'development', gulp.dest(targetDir + "/js")))
+   .pipe(gulpif(options.env === 'production', buffer()))
+   .pipe(gulpif(options.env === 'production', uglify()))
+   .pipe(gulpif(options.env === 'production', rename({suffix: '.min'})))
+   .pipe(gulpif(options.env === 'production', gulp.dest( targetDir + "/js" )));
 });
 
 
-gulp.task('cssdep-dev', ['clean-dev'], function(){
+gulp.task('cssdep', ['clean'], function(){
   var cssFilter = filter("*.css");
   return gulp.src(mainBowerFiles())
   .pipe( cssFilter )
-  .pipe(gulp.dest(devDir + "/css"));
+  .pipe(gulpif(options.env === 'development',gulp.dest(targetDir + "/css")))
+  .pipe(gulpif(options.env === 'production', rename({suffix:'.min'})))
+  .pipe(gulpif(options.env === 'production', minifycss()))
+  .pipe(gulpif(options.env === 'production', gulp.dest( targetDir + "/css")));
 });
 
 
-gulp.task('main-dev', ['clean-dev'], function() {
+gulp.task('main', ['clean'], function() {
   // Browserify/bundle the JS.
   return browserify(paths.main.src)
    .transform(reactify)
    .bundle()
    .pipe(source(paths.main.name))
-   .pipe(gulp.dest( paths.main.dev));
+   //.pipe(gulp.dest( targetDir + "/js"))
+   .pipe(gulpif(options.env === 'production', buffer()))
+   .pipe(gulpif(options.env === 'development', gulp.dest( targetDir + "/js")))
+   .pipe(gulpif(options.env === 'production', uglify()))
+   .pipe(gulpif(options.env === 'production', rename({suffix: '.min'})))
+   .pipe(gulpif(options.env === 'production', gulp.dest( targetDir + "/js")));
 });
 
 
-gulp.task('style-dev',['clean-dev'], function () {
+gulp.task('style',['clean'], function() {
   return gulp.src(paths.style.src)
   .pipe(sass({style:'expanded'}))
-  .pipe(gulp.dest(paths.style.dev))
+  .pipe(gulpif(options.env === 'development', gulp.dest(targetDir + "/css")))
+  .pipe(gulpif(options.env === 'production', rename({suffix:'.min'})))
+  .pipe(gulpif(options.env === 'production', minifycss()))
+  .pipe(gulpif(options.env === 'production',gulp.dest(targetDir + "/css")));
 
 });
 
-gulp.task('templates-dev',['clean-dev'], function(){
-  gulp.src( paths.templates.src )
+gulp.task('templates',['clean'], function(){
+  return gulp.src( paths.templates.src )
   .pipe(handlebars())
   .pipe(wrap('Handlebars.template(<%= contents %>)'))
   .pipe(declare({
@@ -141,63 +177,10 @@ gulp.task('templates-dev',['clean-dev'], function(){
     noRedeclare: true, // Avoid duplicate declarations
   }))
   .pipe(concat( paths.templates.name ))
-  .pipe(gulp.dest( paths.templates.dev ));
+  .pipe(gulp.dest( targetDir + "/js" ));
 });
-
-
-
-
-// Production Tasks
-gulp.task('war', ['clean','html-prod','style-prod', 'main-prod'], function(){
-
-});
-
-
-//gulp.task('clean', function(cb) {
-//  del(['dist'], cb);
-//});
-
-gulp.task('clean-prod', function() {
-  return gulp.src( prodDir + "/*" )
-  .pipe( rm ())
-  .pipe( gulp.dest( prodDir ));
-});
-
-
-gulp.task('html-prod', ['clean-prod'],function () {
-  return gulp.src( paths.html.src)
-  .pipe(wiredep())
-  .pipe(gulp.dest( paths.html.prod));
-});
-
-
-gulp.task('main-prod', function() {
-  // Browserify/bundle the JS.
-  return browserify(paths.main.src)
-  .transform(reactify)
-  .bundle()
-  .pipe(source(paths.main.name))
-  .pipe(buffer())
-  .pipe(gulp.dest( paths.main.prod))
-  .pipe(uglify())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest( paths.main.prod));
-});
-
-gulp.task('style-prod', function () {
-  return gulp.src(paths.style.src)
-  .pipe(sass({style:'expanded'}))
-  .pipe(gulp.dest(paths.style.prod))
-  .pipe(rename({suffix:'.min'}))
-  .pipe(minifycss())
-  .pipe(gulp.dest(paths.style.prod));
-});
-
-
-
-
-
-
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['run', 'html-dev', 'style-dev', 'main-dev', 'watch']);
+gulp.task('default', ['run', 'build', 'style', 'main', 'watch'], function(){
+  console.log( "default run");
+});
